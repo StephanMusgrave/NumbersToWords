d0e3dccc94dca0748d9148cc38502c02d9c1a3ee
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-07-29 18:49:08 +0100

    added hash of unique numbers less than 20

--- lib/number_to_words.rb
+++ lib/number_to_words.rb
@@ -1,12 +1,18 @@
 class Words
 
-  WORDNUMBERS ={2 => "two", 1 => "one"}
-
+  ONESANDTEENS =  { 19 => "nineteen", 18 => "eighteen", 17 => "seventeen", 
+                    16 => "sixteen", 15 => "fifteen", 14 => "fourteen", 
+                    13 => "thirteen", 12 => "twelve", 11 => "eleven",
+                    10 => "ten", 9 => "nine", 8 => "eight", 7 => "seven", 
+                    6 => "six", 5 => "five", 4 => "four", 3 => "three", 
+                    2 => "two", 1 => "one"}
+  TENS =  { 90 => "", 80 => "eighty", 70 => "seventy", 60 => "sixty", 
+            50 => "fifty", 40 => "fourty", 30 => "thirty", 20 => "twenty"}
+  
   def self.convert(number)
     word = ""
-    WORDNUMBERS.each do |key,value|
-      (number / key).times {word << value; number -= key}
-    end
+    word = ONESANDTEENS[number]
+
     word
   end
 
